cc016702c7fdccb6dd26fcf42e0359a5367f3c44
Daniel Tyner-Bryan <dan.tyner.bryan@gmail.com>
2014-05-10 18:53:43 -0400

    adding in groomsmen and bridesmaids routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Meghananddanmobile::Application.routes.draw do
   root :to => 'pages#home'
   match '/weddingparty',   to: 'pages#wedding_party', as: 'wedding_party'
+  match '/groomsmen',   to: 'pages#groomsmen', as: 'groomsmen'
+  match '/bridesmaids',   to: 'pages#bridesmaids', as: 'bridesmaids'
   match '/venue',   to: 'pages#venue', as: 'venue'
   match '/md', to: 'pages#md', as: 'md'
 end
